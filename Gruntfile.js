d26653049336487456954dca943913a522c59bc4
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2014-01-26 16:59:40 +0200

    Added Gruntfile for jshint and mocha tests

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      files: ['Gruntfile.js', '*.js', 'test/**/*.js'],
+      options: {}
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.registerTask('test', ['jshint', 'mochaTest']);
+
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+
+};
