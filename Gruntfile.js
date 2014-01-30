a6db95f5ef37348b0fbd14d7589146b5aff8503a
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2014-01-30 21:33:51 +0200

    Run mocha before jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-test');
 
-  grunt.registerTask('test', ['jshint', 'mochaTest']);
+  grunt.registerTask('test', ['mochaTest', 'jshint']);
 
   grunt.registerTask('default', ['jshint', 'mochaTest']);
 
